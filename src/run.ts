1698df157da378c2aa6a79116a5ae81a19e13675
Walker Flynn <walkerflynn@walker.lan>
2022-12-16 23:23:02 -0800

    it gets the code and uses a global variable to track state changes:

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,12 @@
 import { agent } from './setup'
+import getAirport from './getAirport'
+import getFlightData from './getFlightData'
+import dotenv from 'dotenv'
+
 import { ToadScheduler, SimpleIntervalJob, AsyncTask } from 'toad-scheduler'
+let lastKnown: number[] | null = null
 
 const scheduler = new ToadScheduler()
-import dotenv from 'dotenv'
 import { issueTakeoff } from './issueCreds'
 dotenv.config()
 console.log("process.env.KMS_SECRET_KEY: ", process.env.KMS_SECRET_KEY)
@@ -23,7 +27,31 @@ taskFunc()
 
 const task = new AsyncTask(
     'simple task', 
-    taskFunc,
+    async () => { 
+        // figure out if one of them has taken off / landed
+        // check current vector against last known. should be able to tell if started or stopped moving.s
+        // if so, issue VC from Veramo instance (with did = did:ens:theirplanes.eth)
+        // then send that VC over DIDComm to did:ens:greenbug.eth
+        const latLng = await getFlightData('a835af')
+        if (!lastKnown && !latLng) {
+            // do nothing
+            return
+        }
+        if (latLng && !lastKnown) {
+            // just took off
+            lastKnown = latLng
+            const airport = getAirport(latLng)
+        }
+        if (!latLng && lastKnown) {
+            // just landed
+            lastKnown = null
+            const airport = getAirport(lastKnown!)
+        }
+        if (latLng && lastKnown) {
+            // flying
+            lastKnown = latLng
+        }
+     },
     (err: Error) => { /* handle error here */ }
 )
 const job = new SimpleIntervalJob({ seconds: 60, }, task)
